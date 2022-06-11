45274dd2caa907d244c0304540e6fd2af15ee433
gshohat <gilad.shohat@gmail.com>
2022-06-11 22:22:10 +0200

    - ci@test

--- index.test.ts
+++ index.test.ts
@@ -1,4 +1,4 @@
-import { assert } from "https://deno.land/std@0.143.0/testing/asserts.ts"
+import { assert } from "https://deno.land/std@0.143.0/testing/asserts.ts";
 
 Deno.test('Hello Test', () => {
     assert('Hello');
