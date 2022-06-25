6993435cca4bf67725bafaf4dcb2a65bd7e57d90
gshohat <gilad.shohat@gmail.com>
2022-06-25 16:31:57 +0200

    refactor@modular classes

---
+++ user/user.d.ts
@@ -0,0 +1,40 @@
+import { PlayerClass } from "./user.ts";
+import { EventEmitter } from "../event-emitter/event-emitter.ts";
+
+declare type Events = {
+  like: [string];
+};
+
+export interface IUser {
+  username: string;
+  gender: Gender;
+}
+
+export interface IRating {
+  score: number;
+  k: number;
+}
+
+export declare enum Gender {
+  Male = 0,
+  Female = 1,
+}
+
+export declare enum SwipeDirection {
+  Left = 0,
+  Right = 1,
+}
+
+export declare class User {
+  username: string;
+  gender: Gender;
+  class: PlayerClass;
+  rating: IRating;
+  eventEmitter: EventEmitter<Events>;
+
+  constructor(user: IUser, eventEmitter: EventEmitter<Events>);
+
+  like(user: User): number;
+
+  calcExpectedScore(opponentRatingScore: number): number;
+}
